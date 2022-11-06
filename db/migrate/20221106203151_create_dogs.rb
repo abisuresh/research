c05adadcb82139d0d2a15b6e3bccffe3083b8841
Abbi <aesther2012@gmail.com>
2022-11-06 15:40:30 -0500

    basic setup of rails app

---
+++ db/migrate/20221106203151_create_dogs.rb
@@ -0,0 +1,13 @@
+class CreateDogs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+      t.string :color
+      t.string :owner
+      t.string :age
+
+      t.timestamps
+    end
+  end
+end
